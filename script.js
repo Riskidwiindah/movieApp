20fd9d8864249c4b484262763423504953446809
RiskiDwiIndah <riskidwiindah@upi.edu>
2022-10-12 15:54:44 +0700

    add a fitur search film

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ let formSearch = document.querySelector(".d-flex")
 let searchFilm = document.querySelector(".form-control")
 // let btnSearch = document.querySelector(".btn")
 
-let getAPIFilm =  async () => {
+
+
+async function getAPIFilm () {
     let response = await fetch("https://api.themoviedb.org/3/discover/movie?api_key=6fa3bf2f89aac75d922f34b5b9e20f26&sort_by=popularity.desc")
     let movie = await response.json()
 
-    console.log(movie);
+    // console.log(movie);
 
     let movieData = movie.results
     movieData.forEach((item, index) => {
@@ -29,29 +31,59 @@ let getAPIFilm =  async () => {
         `
     });
 }
-getAPIFilm()
 
 async function searchShow (query){
-    let response = await fetch(`https://api.themoviedb.org/3search/movie?api_key=6fa3bf2f89aac75d922f34b5b9e20f26&query=${query}&page=1`)
+    let response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=6fa3bf2f89aac75d922f34b5b9e20f26&query=${query}&page=1`)
     let movie = await response.json()
 
-    console.log(movie);
+    // console.log(movie);
 
     let movieData = movie.results
     movieData.forEach((item, index) => {
-        cardFilm.innerHTML +=
-        `<div class="col">
-            <div class="card m-5" style="width: 18rem;">
-                <img src="https://image.tmdb.org/t/p/w500/${item.poster_path}" class="card-img-top" alt="poster film ${item.title}">
-                <div class="card-body">
-                    <h5 class="card-title">${item.original_title}</h5>
-                    <h5 class="card-title">${item.vote_average}</h5>
-                    <p class="card-text">${item.release_date}</p>
+        if (item.poster_path == null){
+            cardFilm.innerHTML +=
+            `<div class="col">
+                <div class="card m-5" style="width: 18rem;">
+                    <img src="/pexels-eberhard-grossgasteiger-1287142.jpg" class="card-img-top" alt="poster film ${item.title}">
+                    <div class="card-body">
+                        <h5 class="card-title">${item.original_title}</h5>
+                        <h5 class="card-title">${item.vote_average}</h5>
+                        <p class="card-text">${item.release_date}</p>
+                    </div>
                 </div>
-            </div>
-        </div>`
+            </div>`
+        }else{
+           cardFilm.innerHTML +=
+            `<div class="col">
+                <div class="card m-5" style="width: 18rem;">
+                    <img src="https://image.tmdb.org/t/p/w500/${item.poster_path}" class="card-img-top" alt="poster film ${item.title}">
+                    <div class="card-body">
+                        <h5 class="card-title">${item.original_title}</h5>
+                        <h5 class="card-title">${item.vote_average}</h5>
+                        <p class="card-text">${item.release_date}</p>
+                    </div>
+                </div>
+            </div>` 
+        }   
     });
 }
+
+function show (cari = null){
+    if (cari != null) {
+        formSearch.addEventListener("submit", (event) => {
+        event.preventDefault()
+        
+        cardFilm.innerHTML = " "
+        let query = searchFilm.value
+        searchShow(query)
+        // getAPIFilmSearch(query)
+        })
+    }
+    getAPIFilm()
+} 
+
+show(searchFilm)
+
 // let getAPIFilmSearch = async(query) => {
 //         let response = await fetch(`https://api.themoviedb.org/3search/movie?api_key=6fa3bf2f89aac75d922f34b5b9e20f26&query=${query}&page=1`)
 //         let movie = await response.json()
@@ -74,14 +106,7 @@ async function searchShow (query){
 //         `
 //     });
 //}
-formSearch.addEventListener("submit", (event) => {
-    event.preventDefault()
-    let query = searchFilm.value
-    searchShow(query)
-    // getAPIFilmSearch(query)
-    
-    
-})
+
 
 // getAPIFilm .then(result => {
 //     return result.json()
